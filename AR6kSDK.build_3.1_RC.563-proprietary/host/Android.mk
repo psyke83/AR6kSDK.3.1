d8ea1055c2b5a9efe01f03ad984b588e021fb0d8
B38613 <B38613@freescale.com>
2011-12-21 17:41:00 +0800

    ENGR00159787 wifi: workaround hw2.1.1 copy error
    revert last commit and work around device.bin copy error
    
    Signed-off-by: Zhou Jianzheng <B38613@freescale.com>

--- AR6kSDK.build_3.1_RC.563-proprietary/host/Android.mk
+++ AR6kSDK.build_3.1_RC.563-proprietary/host/Android.mk
@@ -26,14 +26,15 @@ ifneq ($(TARGET_SIMULATOR),true)
 LOCAL_PATH:= $(call my-dir)
 
 define include-ar6k-prebuilt
-    include $(CLEAR_VARS)
+    include $$(CLEAR_VARS)
     LOCAL_MODULE := $(4)
     LOCAL_MODULE_STEM := $(3)
     LOCAL_MODULE_TAGS := debug eng optional
     LOCAL_MODULE_CLASS := ETC
     LOCAL_MODULE_PATH := $(2)
     LOCAL_SRC_FILES := $(1)
-    include $(BUILD_PREBUILT)
+    ALL_DEFAULT_INSTALLED_MODULES += $(LOCAL_MODULE)
+    include $$(BUILD_PREBUILT)
 endef
 
 define add-ar6k-prebuilt-file
@@ -61,36 +62,15 @@ ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/athtcmd_ram.bin
 $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/athtcmd_ram.bin : $(LOCAL_PATH)/../target/AR6003/hw2.0/bin/athtcmd_ram.bin | $(ACP)
 	$(transform-prebuilt-to-target)
 
-# HW2.1.1 firmware
 ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/bdata.SD31.bin
 $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/bdata.SD31.bin : $(LOCAL_PATH)/support/fakeBoardData_AR6003_v2_0.bin | $(ACP)
-        $(transform-prebuilt-to-target)
-ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/athwlan.bin
-$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/athwlan.bin : $(LOCAL_PATH)/../target/AR6003/hw2.1.1/bin/athwlan.bin | $(ACP)
 	$(transform-prebuilt-to-target)
-ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/athwlan_router.bin
-$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/athwlan_router.bin : $(LOCAL_PATH)/../target/AR6003/hw2.1.1/bin/athwlan_router.bin | $(ACP)
-        $(transform-prebuilt-to-target)
-ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/athwlan_mobile.bin
-$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/athwlan_mobile.bin : $(LOCAL_PATH)/../target/AR6003/hw2.1.1/bin/athwlan_mobile.bin | $(ACP)
-        $(transform-prebuilt-to-target)
-ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/athwlan_tablet.bin
-$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/athwlan_tablet.bin : $(LOCAL_PATH)/../target/AR6003/hw2.1.1/bin/athwlan_tablet.bin | $(ACP)
-        $(transform-prebuilt-to-target)
-ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/data.patch.hw3_0.bin
-$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/data.patch.hw3_0.bin : $(LOCAL_PATH)/../target/AR6003/hw2.1.1/bin/data.patch.hw3_0.bin | $(ACP)
-        $(transform-prebuilt-to-target)
-ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/otp.bin
-$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/otp.bin : $(LOCAL_PATH)/../target/AR6003/hw2.1.1/bin/otp.bin | $(ACP)
-        $(transform-prebuilt-to-target)
+
 ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/athtcmd_ram.bin
 $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/athtcmd_ram.bin : $(LOCAL_PATH)/../target/AR6003/hw2.1.1/bin/athtcmd_ram.bin | $(ACP)
-        $(transform-prebuilt-to-target)
-ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/device.bin
-$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1/device.bin : $(LOCAL_PATH)/../target/AR6003/hw2.1.1/bin/device.bin | $(ACP)
-        $(transform-prebuilt-to-target)
+	$(transform-prebuilt-to-target)
 # HW2.1.1 firmware
-ar6k_hw21_src_dir := ../target/AR6003/hw2.1.1/bin/
+ar6k_hw21_src_dir := ../target/AR6003/hw2.1.1/bin
 ar6k_hw21_dst_dir := $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.1.1
 
 $(call add-ar6k-prebuilt-file,$(ar6k_hw21_src_dir)/athwlan.bin,$(ar6k_hw21_dst_dir),athwlan.bin,athwlan221)
@@ -99,13 +79,10 @@ $(call add-ar6k-prebuilt-file,$(ar6k_hw21_src_dir)/athwlan_mobile.bin,$(ar6k_hw2
 $(call add-ar6k-prebuilt-file,$(ar6k_hw21_src_dir)/athwlan_tablet.bin,$(ar6k_hw21_dst_dir),athwlan_tablet.bin,athwlan_tablet221)
 $(call add-ar6k-prebuilt-file,$(ar6k_hw21_src_dir)/data.patch.hw3_0.bin,$(ar6k_hw21_dst_dir),data.patch.hw3_0.bin,athpatch221)
 $(call add-ar6k-prebuilt-file,$(ar6k_hw21_src_dir)/otp.bin,$(ar6k_hw21_dst_dir),otp.bin,athotp221)
-$(call add-ar6k-prebuilt-file,$(ar6k_hw21_src_dir)/athtcmd_ram.bin,$(ar6k_hw21_dst_dir),athtcmd_ram.bin,athtcmd221)
-$(call add-ar6k-prebuilt-file,$(ar6k_hw21_src_dir)/device.bin,$(ar6k_hw21_dst_dir),device.bin,athdevice221)
-$(call add-ar6k-prebuilt-file,support/fakeBoardData_AR6003_v2_0.bin,$(ar6k_hw21_dst_dir),bdata.SD31.bin,athdata221)
+$(call add-ar6k-prebuilt-file,$(ar6k_hw21_src_dir)/device.bin,$(ar6k_hw21_dst_dir),device.bin,athtdevice221)
 
 ar6k_hw21_src_dir :=
 ar6k_hw21_dst_dir :=
 
 include $(LOCAL_PATH)/tools/Android.mk
-
 endif
